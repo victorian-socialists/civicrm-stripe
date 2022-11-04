fd5c8c744c26a5ad911d7011595f143a1f70454c
Matthew Wire <mjw@mjwconsult.co.uk>
2022-11-04 21:53:41 +0000

    Update stripe library to v9

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,33 +1,33 @@
 <?php return array (
   'root' => 
   array (
-    'pretty_version' => 'dev-master',
-    'version' => 'dev-master',
+    'pretty_version' => '6.8.x-dev',
+    'version' => '6.8.9999999.9999999-dev',
     'aliases' => 
     array (
     ),
-    'reference' => '28d24235bc7cc88a7ef4b1fd6953cb9700940f14',
+    'reference' => 'dfa91b55d5f8b94ef3247c96ba99402d622e0f9e',
     'name' => 'civicrm/stripe',
   ),
   'versions' => 
   array (
     'civicrm/stripe' => 
     array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
+      'pretty_version' => '6.8.x-dev',
+      'version' => '6.8.9999999.9999999-dev',
       'aliases' => 
       array (
       ),
-      'reference' => '28d24235bc7cc88a7ef4b1fd6953cb9700940f14',
+      'reference' => 'dfa91b55d5f8b94ef3247c96ba99402d622e0f9e',
     ),
     'stripe/stripe-php' => 
     array (
-      'pretty_version' => 'v7.128.0',
-      'version' => '7.128.0.0',
+      'pretty_version' => 'v9.8.0',
+      'version' => '9.8.0.0',
       'aliases' => 
       array (
       ),
-      'reference' => 'c704949c49b72985c76cc61063aa26fefbd2724e',
+      'reference' => '5a55767326e77dac8a0f57022d27533beeafc197',
     ),
   ),
 );
