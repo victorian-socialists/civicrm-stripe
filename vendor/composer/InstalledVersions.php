6207f498dd445f5bb507c3ea0bad77a544773e30
Matthew Wire <mjw@mjwconsult.co.uk>
2023-02-27 13:51:47 +0000

    Upgrade stripe library from 9.8.0 to 9.9.0

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -25,33 +25,33 @@ class InstalledVersions
 private static $installed = array (
   'root' => 
   array (
-    'pretty_version' => '6.8.x-dev',
-    'version' => '6.8.9999999.9999999-dev',
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
     'aliases' => 
     array (
     ),
-    'reference' => 'dfa91b55d5f8b94ef3247c96ba99402d622e0f9e',
+    'reference' => 'b9b8fdaea5e0876d35a29274c9261a6a4c60f160',
     'name' => 'civicrm/stripe',
   ),
   'versions' => 
   array (
     'civicrm/stripe' => 
     array (
-      'pretty_version' => '6.8.x-dev',
-      'version' => '6.8.9999999.9999999-dev',
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
       'aliases' => 
       array (
       ),
-      'reference' => 'dfa91b55d5f8b94ef3247c96ba99402d622e0f9e',
+      'reference' => 'b9b8fdaea5e0876d35a29274c9261a6a4c60f160',
     ),
     'stripe/stripe-php' => 
     array (
-      'pretty_version' => 'v9.8.0',
-      'version' => '9.8.0.0',
+      'pretty_version' => 'v9.9.0',
+      'version' => '9.9.0.0',
       'aliases' => 
       array (
       ),
-      'reference' => '5a55767326e77dac8a0f57022d27533beeafc197',
+      'reference' => '479b5c2136fde0debb93d290ceaf20dd161c358f',
     ),
   ),
 );
